cba1f2beee972fbe632e3da9e1ce421c1e76f121
wikli <wikli@nik.gov.pl>
2020-11-05 12:14:37 +0100

    main.sql modified

--- main.sql
+++ main.sql
@@ -1,3 +1,4 @@
+new line added in branch 'MASTER'
 
 association = createAssociation(i"http://www.rodan.pl/psi/association-class/association-account-group-member", null);
     role = createRole(association, i"http://www.rodan.pl/xtm/cgm.xtm#member_container");
